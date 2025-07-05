9a00a22eaa172b8c3614e175a6da00c34917a5f9
Bilal Gurani <bilalgurani@Bilals-MacBook-Pro.local>
2025-07-05 16:30:21 +0530

    Add env variables, compression

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ const bodyParser = require("body-parser");
 const csrf = require("csurf");
 const flash = require('connect-flash');
 const multer = require('multer');
+const helmet = require('helmet');
+const compression = require('compression');
+const morgan = require('morgan');
+const fs = require('fs');
+
 
 const session = require('express-session');
 
@@ -14,7 +19,7 @@ const shopRoutes = require("./routes/shop");
 const authRoutes = require("./routes/auth");
 
 const store = new MongoDBStore({
-  uri: 'mongodb+srv://bilalahamedgurani:Bilal1khan@cluster0.btxkbsv.mongodb.net/shop?retryWrites=true&w=majority&appName=Cluster0',
+  uri: `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.btxkbsv.mongodb.net/${process.env.MONGO_DEFAULT_DATABASE}?retryWrites=true&w=majority&appName=Cluster0`,
   collection: "sessions"
 })
 
@@ -47,6 +52,12 @@ const app = express();
 
 app.set("view engine", "ejs");
 app.set("views", "views");
+
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), {flags: 'a'})
+
+app.use(helmet());
+app.use(compression());
+app.use(morgan('combined', {stream: accessLogStream}));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(multer({storage: fileStorage, fileFilter: fileFilter}).single('image'));
@@ -93,6 +104,6 @@ app.use(authRoutes);
 app.use(errorController.get404);
 
 mongoConnect(() => {
-  app.listen(3000); 
+  app.listen(process.env.PORT || 3000); 
 })
 
